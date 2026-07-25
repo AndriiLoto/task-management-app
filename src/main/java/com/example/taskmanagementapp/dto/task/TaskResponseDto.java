df124b07519ae68e138f681f94cfc442ecf3e8af
wetwe <wetwettew@yahoo.com>
2026-07-25 14:58:01 +0300

    Added ManyToMany relationship for labels in Task entity

--- src/main/java/com/example/taskmanagementapp/dto/task/TaskResponseDto.java
+++ src/main/java/com/example/taskmanagementapp/dto/task/TaskResponseDto.java
@@ -1,8 +1,10 @@
 package com.example.taskmanagementapp.dto.task;
 
+import com.example.taskmanagementapp.dto.label.LabelResponseDto;
 import com.example.taskmanagementapp.model.TaskPriority;
 import com.example.taskmanagementapp.model.TaskStatus;
 import java.time.LocalDateTime;
+import java.util.Set;
 import lombok.Getter;
 import lombok.Setter;
 import lombok.ToString;
@@ -19,4 +21,5 @@ public class TaskResponseDto {
     private LocalDateTime dueDate;
     private Long projectId;
     private Long assigneeId;
+    private Set<LabelResponseDto> labels;
 }
