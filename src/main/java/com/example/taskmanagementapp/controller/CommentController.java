55a7af08bb144ef6c752f20d926fd56389c72e5e
wetwe <wetwettew@yahoo.com>
2026-07-25 14:14:12 +0300

    Added endpoint to create comment

---
+++ src/main/java/com/example/taskmanagementapp/controller/CommentController.java
@@ -0,0 +1,35 @@
+package com.example.taskmanagementapp.controller;
+
+import com.example.taskmanagementapp.dto.comment.CommentResponseDto;
+import com.example.taskmanagementapp.dto.comment.CreateCommentRequestDto;
+import com.example.taskmanagementapp.model.User;
+import com.example.taskmanagementapp.service.CommentService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/comments")
+@RequiredArgsConstructor
+@Tag(name = "Comment", description = "Endpoints for comment management")
+public class CommentController {
+    private final CommentService commentService;
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    @Operation(summary = "Create new comment",
+            description = "Creates a new comment for a task"
+    )
+    public CommentResponseDto createComment(@AuthenticationPrincipal User user,
+                                            @RequestBody @Valid CreateCommentRequestDto requestDto) {
+        return commentService.createComment(user, requestDto);
+    }
+}
