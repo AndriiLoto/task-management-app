784d37add00cd225d186c46085fdcc32fbc8e4b5
wetwe <wetwettew@yahoo.com>
2026-08-03 16:46:53 +0300

    added initial database setup for tests

---
+++ src/test/resources/database/add-comments.sql
@@ -0,0 +1,2 @@
+INSERT INTO comments (id, task_id, user_id, text, timestamp)
+VALUES (1, 1, 2, 'Initial dummy comment', '2026-08-03 12:00:00');
---
+++ src/test/resources/database/add-projects.sql
@@ -0,0 +1,10 @@
+INSERT INTO projects (id, name, description, start_date, end_date, status, user_id)
+VALUES (
+    1,
+    'Seed Project',
+    'Project for repository tests',
+    '2026-08-01 09:00:00',
+    '2026-08-31 18:00:00',
+    'INITIATED',
+    1
+);
